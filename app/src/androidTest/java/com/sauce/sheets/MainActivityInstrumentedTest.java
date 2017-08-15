5466ad189d89ad1b2d321959c2e3f98da799a04e
Brando Madden <brando.madden@gmail.com>
2017-08-14 22:04:16 -0700

    added tests

--- app/src/androidTest/java/com/sauce/sheets/MainActivityInstrumentedTest.java
+++ app/src/androidTest/java/com/sauce/sheets/MainActivityInstrumentedTest.java
@@ -13,7 +13,14 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isEnabled;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -23,6 +30,10 @@ import static org.junit.Assert.assertEquals;
  */
 @RunWith(AndroidJUnit4.class)
 public class MainActivityInstrumentedTest {
+
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
@@ -38,15 +49,24 @@ public class MainActivityInstrumentedTest {
 
     @Test
     public void changeText_sameActivity() {
-        // Type text and then press the button.
+        // Select first cell and verify text can be typed into it
+
+        //Verify Editcell is disabled by itself
+        onView(withId(R.id.main_cell_edit_area)).check(matches(not(isEnabled())));
+
+        //Select first cell in table
         onView(withId(R.id.main_recycler_view))
                 .perform(actionOnItemAtPosition(0, click()));
-        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.main_cell_edit_area)).perform(click());
 
-        // Check that the text was changed.
-        onView(withId(R.id.textToBeChanged))
-                .check(matches(withText(mStringToBetyped)));
+        //Verify Editcell is now enabled
+        onView(withId(R.id.main_cell_edit_area)).check(matches(isEnabled()));
+        //Type text into edit cell
+        onView(withId(R.id.main_cell_edit_area)).perform(typeTextIntoFocusedView("Form"));
+        //Check that input shows in spreadsheet cell
+        onView(withRecyclerView(R.id.main_recycler_view).atPosition(0))
+                .check(matches(hasDescendant(withText("Form"))));
+
     }
-
-
 }
+
